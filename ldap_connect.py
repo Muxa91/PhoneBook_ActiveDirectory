09a62bbc255a3af297741f220b4809ba0ebbd4c9
muxa <franzus91@gmail.com>
2023-10-16 11:54:00 +0300

    added column/search mobile phoned AD, reset button

--- ldap_connect.py
+++ ldap_connect.py
@@ -9,7 +9,7 @@ server = Server(userpass.ip, get_info=ALL)
 conn = Connection(server, read_only=True, user=userpass.user, password=userpass.password)
 conn.bind()
 # IpPhone
-conn.search('dc=eskaro,dc=net', '(&(objectclass=person)(telephoneNumber=*))',attributes=['sn','givenName','title','telephoneNumber','mail'])
+conn.search('dc=eskaro,dc=net', '(&(objectclass=person)(!(userAccountControl=514))(|(telephoneNumber=*)(Mobile=*)))',attributes=['sn','givenName','description','telephoneNumber','Mobile','mail'])
 
 employees=[]
 result=[]
@@ -18,17 +18,16 @@ def list():
         for x in conn.entries:
             firsname =' ' if x['sn'] == None else x['sn'][0]
             givenName = ' ' if x['givenName'] == None else x['givenName'][0]
-            title = ' ' if x['title'] == None else x['title'][0]
+            title = ' ' if x['description'] == None else x['description'][0]
             IpPhone = ' ' if x['telephoneNumber'] == None else x['telephoneNumber'][0]
+            mobile = ' ' if x['Mobile'] == None else x['Mobile'][0]
             mail = ' ' if x['mail'] == None else x['mail'][0]
 
-            employees.append([firsname,givenName,IpPhone,title,mail])
+            employees.append([firsname,givenName,IpPhone,title,mobile,mail])
             result=sorted(employees)
 
         for x in result:
-            file.write(x[0] + " " + x[1] + ";" + x[2] + ";" + x[3] + ";"+x[4] + "|")
-
-
+            file.write(x[0] + " " + x[1] + ";" + x[2] + ";" + x[3] + ";"+x[4] +";"+ x[5] + "|")
 
 
 def person():
